1604543c768c6120200596c7226e44af3fc453da
Sebastian Galvis <jsgalvish@unal.edu.co>
2020-04-29 17:48:58 -0500

    net

---
+++ InventoryWebApi/Controllers/ItemController.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using InventoryWebApi.DataAccess;
+using InventoryWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using InventoryWebApi.DTO;
+
+namespace InventoryWebApi.Controllers
+{
+}
---
+++ InventoryWebApi/DataAccess/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using InventoryWebApi.Models;
+
+namespace InventoryWebApi.DataAccess
+{
+    public interface IRepository
+    {
+        Task<List<InventoryItem>> GetInventory();
+        Task<InventoryItem> AddInventoryItem(InventoryItem inventoryItem);
+        Task<List<InventoryItem>> GetInventoryItem(ItemQueryModel query);
+        Task<List<InventoryItem>> GetSorted();
+        Task<InventoryItem> UpdateInventoryItem(InventoryItem inventoryItem);
+        Task<InventoryItem> DeleteInventoryItem(InventoryItem inventoryItem);
+    }
+}
